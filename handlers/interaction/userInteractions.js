fbc0678979ea31c86f129246074d0815354aadab
NickNinjaGit <balguing@gmail.com>
2025-02-17 23:44:45 -0300

    fixed error

--- handlers/interaction/userInteractions.js
+++ handlers/interaction/userInteractions.js
@@ -285,11 +285,9 @@ async function StartBattle(interaction, activeInteractions) {
   })
 
   collector.on("collect", async (i) => {
-    if(i.c)
+    
   })
-
-  
-  
 }
+  
 
 module.exports = { myProfile, friendProfile, Work, Daily, Shop, Leaderboards };
