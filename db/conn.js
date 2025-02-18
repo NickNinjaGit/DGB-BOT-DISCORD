b7cd9e6ae5bc22149fd988973f2c96e13e9eb9a5
NickNinjaGit <balguing@gmail.com>
2025-02-18 10:38:07 -0300

    changing db settings

--- db/conn.js
+++ db/conn.js
@@ -4,7 +4,7 @@ const conn = new Sequelize({
     dialect: 'mysql',
     host: 'localhost',
     username: 'root',
-    password: "",
+    password: process.env.DB_PASSWORD,
     database: 'dgbbot_database',
     logging: false
 });
