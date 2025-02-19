e06c50e135c59c61a3a9191b32b1519888d7a665
NickNinjaGit <balguing@gmail.com>
2025-02-19 11:40:28 -0300

    fixing active interactions

--- helpers/checkActiveInteractions.js
+++ helpers/checkActiveInteractions.js
@@ -1,6 +1,7 @@
 module.exports = async function checkActiveInteractions(discordID, interaction, activeInteractions) {
     if (activeInteractions.has(discordID)) {
-        await interaction.reply({
+        await interaction.deferReply({ ephemeral: true });
+        await interaction.editReply({
           content: "Você já possui uma interação ativa! Aguarde ou finalize antes de executar novamente.",
         });
         await interaction.deleteReply();
